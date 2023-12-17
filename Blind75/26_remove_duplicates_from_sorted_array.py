2a553b9d087a141ba01a43889d87f75a72c5ef77
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2023-12-17 17:49:13 -0500

    26 remove dplicates from array

--- Blind75/26_remove_duplicates_from_sorted_array.py
+++ Blind75/26_remove_duplicates_from_sorted_array.py
@@ -1,1 +1,20 @@
-two pointer solution
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = 0
+
+        for r in range(len(nums)):
+          if nums[r] != nums[r-1]:
+            print(r)
+            nums[l] = nums[r]
+            l +=1
+        return l
+    
+
+    # 2 pointer solution moving left adn right poiinters
+    # check right pointer as you move along with r-1 cause you will run into duplicates in the list with l if not
+    # checking l and r in place will get you duplicates but checking r with r-1 will not
+          

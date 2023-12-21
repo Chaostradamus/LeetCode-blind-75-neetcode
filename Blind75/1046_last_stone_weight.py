4db9a6af454a07f136150878328224e9a9564cdb
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2023-12-21 01:59:09 -0500

    last stone weight done

--- Blind75/1046_last_stone_weight.py
+++ Blind75/1046_last_stone_weight.py
@@ -4,7 +4,27 @@ class Solution(object):
         :type stones: List[int]
         :rtype: int
         """
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+          first = heapq.heappop(stones)
+          second = heapq.heappop(stones)
+          if second > first:
+            heapq.heappush(stones, first - second)
         
+        stones.append(0)
+        return abs(stones[0])
+
+        
+
+
+# use maxheap and pop first 2 and compare. push difference into heap and return last stone standing
+#  append 0 before returning in case of edge case of empty stones list
+# python doesnt have max heap so we make everything negative at first
+# we do if second greater than first because if theyre equal theyll both be popped already so basically deleted
+# we find difference between first and second because subtraction will turn second one positive to get proper difference
+
 
 
 

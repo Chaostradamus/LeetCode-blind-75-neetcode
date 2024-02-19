93ce8c77c92b597e283d85ed6f10944a53249196
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-02-18 23:36:40 -0500

    histogram python tutor added

--- Blind75/84_largest_histogram.py
+++ Blind75/84_largest_histogram.py
@@ -15,6 +15,7 @@ class Solution:
             maxArea = max(maxArea, h * (len(heights) - i))
         return maxArea
 
+
 84. Largest Rectangle in Histogram
 Hard
 Topics
@@ -38,3 +39,5 @@ Output: 4
 
 # should be similar to water container algorithm but with stack usage
  
+
+ https://pythontutor.com/render.html#code=class%20Solution%28object%29%3A%0A%20%20%20%20def%20largestRectangleArea%28self,%20heights%29%3A%0A%20%20%20%20%20%20%20%20maxArea%20%3D%200%0A%20%20%20%20%20%20%20%20stack%20%3D%20%5B%5D%20%20%23%20pair%3A%20%28index,%20height%29%0A%0A%20%20%20%20%20%20%20%20for%20i,%20h%20in%20enumerate%28heights%29%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20start%20%3D%20i%0A%20%20%20%20%20%20%20%20%20%20%20%20while%20stack%20and%20stack%5B-1%5D%5B1%5D%20%3E%20h%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20index,%20height%20%3D%20stack.pop%28%29%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20maxArea%20%3D%20max%28maxArea,%20height%20*%20%28i%20-%20index%29%29%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20start%20%3D%20index%0A%20%20%20%20%20%20%20%20%20%20%20%20stack.append%28%28start,%20h%29%29%0A%0A%20%20%20%20%20%20%20%20for%20i,%20h%20in%20stack%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20maxArea%20%3D%20max%28maxArea,%20h%20*%20%28len%28heights%29%20-%20i%29%29%0A%20%20%20%20%20%20%20%20return%20maxArea%0A%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%0Ab%20%3D%20Solution%28%29%0Ab.largestRectangleArea%28%5B2,1,5,6,2,3%5D%29&cumulative=false&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=2&rawInputLstJSON=%5B%5D&textReferences=false
